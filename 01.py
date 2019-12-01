8eb5bd4116eb32a77509f9f7c5cbff56a9b51d65
Marcus Frodin <marcus.frodin@gmail.com>
2019-12-01 07:10:03 -0500

    First day done

---
+++ 01.py
@@ -0,0 +1,16 @@
+from math import floor
+input = [int(row) for row in open('01.input', 'r').read().split("\n")]
+def fuel(n): return floor(n/3)-2
+
+# A
+print(sum([fuel(n) for n in input]))
+
+# B
+total=0
+for row in input:
+    remaining = row
+    while remaining>0:
+        remaining = fuel(remaining)
+        total+=max(0, remaining)
+
+print(total)
